eac929c9e6f6667f8afd0aecfcb720ad9eeb7400
BVanBuren <bvanburenwx@gmail.com>
2021-07-04 18:57:22 -0400

    Added variables to the javascript file. Also added a function that would select a timer class from HTML and start counting down a timer from 75 seconds. I also styled the timer div to be on the right side of the page and follow the color scheme that was planned out.

---
+++ Assets/script.js
@@ -0,0 +1,20 @@
+// select element by class for the timer 
+var timeEL = document.querySelector(".timeCounter");
+
+var secondsLeft = 75;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEL.textContent = secondsLeft + " seconds left.";
+
+        if(secondsLeft === 0) {
+            
+            clearInterval(timerInterval);
+
+            sendMessage();
+        }
+    }, 1000);
+}
+
+console.log(setTime())
