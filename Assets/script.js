42d98ddadff66d8e5954e7f1b70c0001789544cf
BVanBuren <bvanburenwx@gmail.com>
2021-07-05 15:26:06 -0400

    added question variable to include all questions and possible choices and correct answer.

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,36 @@
 // select element by class for the timer 
 var timeEL = document.querySelector(".timeCounter");
-var start = document.querySelector("#startQuiz");
+// select element by ID for the startQuiz bttn. 
+var startQuiz = document.getElementById("#startQuiz");
+
+// variable array for questions and answers.
+var quizQuestions = [
+    {
+        question1: "What does DOM stand for?",
+        choices:["Distributed Object Management", "Document Object Model", "Digital Output Module", "Document Object Management"],
+        answer: "Document Object Model"
+    },
+    {
+        question2: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<js>," "<javascript>", "<style>", "<script>"],
+        answer: "<script>"
+    },
+    {
+        question3: "How do you write "Hello World" in an alert box?", 
+        choices: ["alert("Hello World")", "msg("Hello World")", "confirm("Hello World")", "msgBox("Hello World")"],
+        answer: "alert("Hello World")"
+    },
+    {
+        question4: "Commonly used data types DO NOT include:", 
+        choices:["strings", "booleans", "alerts", "numbers"], 
+        answer: "alerts"
+    },
+    {
+        question5: "How does a FOR loop start?",
+        choices:["for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i = 0; i <= 5)" "for (i <= 5; i++)"],
+        answer: "for (i = 0; i <= 5; i++)"
+    },
+];
 
 var secondsLeft = 75;
 
@@ -12,10 +42,6 @@ function setTime() {
         if(secondsLeft === 0) {
             
             clearInterval(timerInterval);
-
-            sendMessage();
-        }
+        } 
     }, 1000);
 }
-console.log(setTime());
-start.addEventListener(click, setTime());
