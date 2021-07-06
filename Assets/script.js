3a72050397f9d806c458f183db3dc8a955499bfc
BVanBuren <bvanburenwx@gmail.com>
2021-07-06 13:53:25 -0400

    fixed time interval to start and decrease the time when the start quiz button is pushed.

--- Assets/script.js
+++ Assets/script.js
@@ -1,47 +1,58 @@
 // select element by class for the timer 
 var timeEL = document.querySelector(".timeCounter");
 // select element by ID for the startQuiz bttn. 
-var startQuiz = document.getElementById("#startQuiz");
+var startQuiz = document.querySelector(".startQuiz");
+// selecting the questions element from HTML 
+var questions = document.querySelector("#questionChoices");
+// selecting the ul element to display the questions from the questions array
+var createList = document.createElement("ul");
+
+startQuiz.addEventListener("click", setTime);
+
+
+var questionNumber = 0;
 
 // variable array for questions and answers.
 var quizQuestions = [
     {
-        question1: "What does DOM stand for?",
+        question: "What does DOM stand for?",
         choices:["Distributed Object Management", "Document Object Model", "Digital Output Module", "Document Object Management"],
         answer: "Document Object Model"
     },
     {
-        question2: "Inside which HTML element do we put the JavaScript?",
+        question: "Inside which HTML element do we put the JavaScript?",
         choices: ["<js>", "<javascript>", "<style>", "<script>"],
         answer: "<script>"
     },
     {
-        question3: 'How do you write "Hello World" in an alert box?', 
+        question: 'How do you write "Hello World" in an alert box?', 
         choices: ['alert("Hello World")','msg("Hello World")','confirm("Hello World")','msgBox("Hello World")'],
         answer: 'alert("Hello World")'
     },
     {
-        question4: "Commonly used data types DO NOT include:", 
+        question: "Commonly used data types DO NOT include:", 
         choices:["strings", "booleans", "alerts", "numbers"], 
         answer: "alerts"
     },
     {
-        question5: "How does a FOR loop start?",
+        question: "How does a FOR loop start?",
         choices:["for (i = 0; i <= 5; i++)", "for i = 1 to 5", "for (i = 0; i <= 5)", "for (i <= 5; i++)"],
         answer: "for (i = 0; i <= 5; i++)"
     },
 ];
 
-var secondsLeft = 75;
+var secondsLeft = 76;
+var startInterval = 0
+
 
 function setTime() {
     var timerInterval = setInterval(function() {
         secondsLeft--;
         timeEL.textContent = secondsLeft + " seconds left.";
 
-        if(secondsLeft === 0) {
-            
+        if(secondsLeft <= 0 || questionNumber === quizQuestions.length) {
             clearInterval(timerInterval);
+            getScore()
         } 
     }, 1000);
 }
